2651210e9945beab9c53b9f04a8446513468bd39
Julian <julixnj31@gmail.com>
2026-02-27 17:52:24 -0500

    Terminacion de la guia

---
+++ 7.js
@@ -0,0 +1,5 @@
+let numero = parseFloat(prompt("Ingresa un número:"));
+let doble = numero * 2;
+let triple = numero * 3;
+console.log("El doble de " + numero + " es: " + doble);
+console.log("El triple de " + numero + " es: " + triple);
